1f01ac6300b7bb8899d3011609f7a92e55edd91b
drakaz <drakpub@gmail.com>
2010-11-07 19:15:05 +0100

    Switch to 6.5
    Change-Id: Ie177e6f335114c94a040c4a95c6dbd5f50120f8b

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@ LOCAL_MODULE := recovery
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
-RECOVERY_API_VERSION := V6.4
+RECOVERY_API_VERSION := V6.5
 LOCAL_CFLAGS += -DRECOVERY_API_VERSION=$(RECOVERY_API_VERSION)
 
 # This binary is in the recovery ramdisk, which is otherwise a copy of root.
